6a38470a1339fe3583ffef90eb847c6d2d676427
vastlaan <vastlaan@mail.com>
2020-05-04 18:41:05 +0200

    allow origin my ip - temporary

--- index.js
+++ index.js
@@ -8,11 +8,29 @@ const app = express();
 app.use(bodyParser.json());
 //Allow Cross Origin from michalantczak.com and my ip
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "145.130.21.226"); // update to match the domain you will make the request from
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+    // Website you wish to allow to connect
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://www.michalantczak.com"
     );
+
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
     next();
 });
 
